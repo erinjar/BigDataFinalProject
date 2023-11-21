2e0aa19eaf702b6cc438b755a277afa2289e8a7b
erinjar <erinjarvis2021@gmail.com>
2023-11-21 11:25:53 +0100

    Made notes file.

--- main.py
+++ main.py
@@ -14,7 +14,21 @@ def get_movie_by_imdb_id(imdb_id):
     else:
         print(f"Error: {response.status_code} - {response.json()['description']}")
 
+def get_movies_by_title(title):
+    url = f"{base_url}getMoviesByTitle?title={title}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        movies = response.json()
+        for movie in movies:
+            if(movie['title'] == title):
+                print(f"Title: {movie['title']}, Rating: {movie['rating']}")
+    else:
+        print(f"Error: {response.status_code} - {response.json()['description']}")
+
+
 
 # Example use
-legally_blonde_id = "0250494"
-get_movie_by_imdb_id(legally_blonde_id)
+# legally_blonde_id = "0250494"
+get_movie_by_imdb_id("0111161")
+get_movies_by_title("Titanic")
