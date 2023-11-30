245c67de92cc541e7870d4c86664f76575bfa046
neel <troegern@whitman.edu>
2023-11-30 01:37:31 +0100

    added data visualization stuff for word frequency

--- wordcloudVisualizer.py
+++ wordcloudVisualizer.py
@@ -33,15 +33,15 @@ for word, freq in word_freq.most_common()[:-11:-1]:
 # generate word cloud
 wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_freq)
 
-# predefining bins
-bins = [1, 5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700]
+# predefining buckets
+buckets = [1, 5, 10, 15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700]
 
-# count words falling into each bucket
-bin_counts = {f'{start}-{end}': sum(1 for freq in word_freq.values() if start <= freq <= end) for start, end in zip(bins, bins[1:])}
+# count words falling into each buckets
+bucket_counts = {f'{start}-{end}': sum(1 for freq in word_freq.values() if start <= freq <= end) for start, end in zip(buckets, buckets[1:])}
 
 # display the bucket counts
 print("\nBucket Counts:")
-for bucket, count in bin_counts.items():
+for bucket, count in bucket_counts.items():
     print(f"{bucket}: {count} words")
 
 # create subplots
@@ -53,7 +53,7 @@ ax1.axis('off')
 ax1.set_title('Word Cloud')
 
 # plot the histogram of word frequencies
-ax2.bar(bin_counts.keys(), bin_counts.values(), color='skyblue')
+ax2.bar(bucket_counts.keys(), bucket_counts.values(), color='skyblue')
 ax2.set_title('Word Frequency Distribution')
 ax2.set_xlabel('Frequency')
 ax2.set_yscale('log')
